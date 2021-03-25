613d6aed5c1671692e02bf3802c4db676729c07f
Milan Miletic <milanmiletic1996@gmail.com>
2021-03-25 14:29:36 +0100

    Making small changes in code and css

--- presets/variable/preset_css_inject.php
+++ presets/variable/preset_css_inject.php
@@ -10,19 +10,26 @@ if ($form_options['button_border_radius'] != '' && $form_options['button_border_
 }
 
 if ($form_options['first_background_color'] != '' && $form_options['first_background_color'] != null) {
-    echo '#free-quote { background-color: rgb(' . $form_options['first_background_color'] . ') !important}';
+    echo '.first_form { background-color: rgb(' . $form_options['first_background_color'] . ') !important}';
 }
 
 if ($form_options['second_background_color'] != '' && $form_options['second_background_color'] != null) {
-    echo '#free-quote { background-color: rgb(' . $form_options['second_background_color'] . ') !important}';
+    echo '.second_form { background-color: rgb(' . $form_options['second_background_color'] . ') !important}';
 }
 
 if ($form_options['first_input_color'] != '' && $form_options['first_input_color'] != null) {
-    echo '#free-quote .form-control { background-color: rgb(' . $form_options['first_input_color'] . ') !important}';
+    echo '.first_form .form-control { background-color: rgb(' . $form_options['first_input_color'] . ') !important}';
+    echo '.first_form .select2-container--default .select2-selection--single { background-color: rgb(' . $form_options['first_input_color'] . ') !important}';
+
 }
 
 if ($form_options['second_input_color'] != '' && $form_options['second_input_color'] != null) {
-    echo '#free-quote .form-control { background-color: rgb(' . $form_options['second_input_color'] . ') !important}';
+    echo '.second_form .form-control { background-color: rgb(' . $form_options['second_input_color'] . ') !important}';
+    echo '.second_form  .select2-container--default .select2-selection--single { background-color: rgb(' . $form_options['first_input_color'] . ') !important}';
 }
 
+
+
+
 ?>
+
