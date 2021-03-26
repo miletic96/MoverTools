2a33a24d5bebe3fed7c61d56fab6a93aa475950f
Milan Miletic <milanmiletic1996@gmail.com>
2021-03-26 16:38:32 +0100

    Small changes

--- mover-quotes.php
+++ mover-quotes.php
@@ -1,31 +1,33 @@
 <?php
 
 /**
-* Plugin Name: Mover Quotes
-* Description: Create forms for moving services easy
-* Version: 1.2.3
-* Author: Milan Miletic
-* License:           GNU General Public License v2
-* License URI:       http://www.gnu.org/licenses/gpl-2.0.html
-**/
+ * Plugin Name: Mover Quotes
+ * Description: Create forms for moving services easy
+ * Version: 1.2.3
+ * Author: Milan Miletic
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ **/
 
 
-function movers_admin_menu_option(){
+function movers_admin_menu_option()
+{
     add_menu_page('movers free quote', 'Movers Forms', 'manage_options', 'movers-forms', 'movers_free_quote_page', '', 200);
 }
 
 add_action('admin_menu', 'movers_admin_menu_option');
 
 
-function movers_free_quote_page(){
-    
+function movers_free_quote_page()
+{
+
     global $wpdb;
     $table_name = $wpdb->prefix . 'movers_form_v2';
 
-    if(array_key_exists('submit', $_POST)){
+    if (array_key_exists('submit', $_POST)) {
         $post_data = array(
-            'two_step_form' => (isset($_POST['two_step_form'])? '1' : '0'),
-            'two_step_form_only_mobile' => (isset($_POST['two_step_form_only_mobile'])? '1' : '0'),
+            'two_step_form' => (isset($_POST['two_step_form']) ? '1' : '0'),
+            'two_step_form_only_mobile' => (isset($_POST['two_step_form_only_mobile']) ? '1' : '0'),
             'redirect_url' => $_POST['redirect_url'],
             'company_id' => $_POST['company_id'],
             'api_url' => $_POST['api_url'],
@@ -49,10 +51,10 @@ function movers_free_quote_page(){
 
     //$form_options = (array) $wpdb->get_results( "SELECT * FROM $table_name WHERE id = 1" )[0];
     $form_options = get_option('movers_quote_options');
-    if($form_options == false){
+    if ($form_options == false) {
         $form_options = array(
-            'two_step_form' => (isset($_POST['two_step_form'])? '1' : '0'),
-            'two_step_form_only_mobile' => (isset($_POST['two_step_form_only_mobile'])? '1' : '0'),
+            'two_step_form' => (isset($_POST['two_step_form']) ? '1' : '0'),
+            'two_step_form_only_mobile' => (isset($_POST['two_step_form_only_mobile']) ? '1' : '0'),
             'redirect_url' => '',
             'company_id' => '',
             'api_url' => '',
@@ -75,19 +77,20 @@ function movers_free_quote_page(){
     include 'movers-admin-panel.php';
 }
 
-function movers_form_install(){
-
+function movers_form_install()
+{
 }
 
-register_activation_hook( __FILE__, 'movers_form_install' );
+register_activation_hook(__FILE__, 'movers_form_install');
 
-function generate_free_quote_form() {
+function generate_free_quote_form()
+{
     $form_options = get_option('movers_quote_options');
-    if($form_options == false)
+    if ($form_options == false)
         return 'free quote form options not set.';
 
     ob_start(); // start a buffer
-    switch($form_options['form_preset']){
+    switch ($form_options['form_preset']) {
         case 0:
             include 'presets/preset_0.php';
             include 'presets/variable/first_form_class.php';
@@ -108,6 +111,10 @@ function generate_free_quote_form() {
             include 'presets/preset_4.php';
             include 'presets/variable/first_form_class.php';
             break;
+        case 5:
+            include 'presets/preset_5.php';
+            include 'presets/variable/first_form_class.php';
+            break;
         default:
             include 'presets/preset_1.php';
             include 'presets/variable/first_form_class.php';
@@ -120,13 +127,14 @@ function generate_free_quote_form() {
 
 add_shortcode('free-quote', 'generate_free_quote_form');
 
-function generate_free_quote_form2() {
+function generate_free_quote_form2()
+{
     $form_options = get_option('movers_quote_options');
-    if($form_options == false)
+    if ($form_options == false)
         return 'free quote form options not set.';
 
     ob_start(); // start a buffer
-    switch($form_options['form_preset2']){
+    switch ($form_options['form_preset2']) {
         case 0:
             include 'presets/preset_0.php';
             include 'presets/variable/second_form_class.php';
@@ -151,10 +159,6 @@ function generate_free_quote_form2() {
             include 'presets/preset_5.php';
             include 'presets/variable/second_form_class.php';
             break;
-        case 6:
-            include 'presets/preset_6.php';
-            include 'presets/variable/second_form_class.php';
-            break;
         default:
             include 'presets/preset_1.php';
             include 'presets/variable/second_form_class.php';
@@ -167,15 +171,15 @@ function generate_free_quote_form2() {
 
 add_shortcode('free-quote2', 'generate_free_quote_form2');
 
-function load_plugin_css() {
-    $plugin_url = plugin_dir_url( __FILE__ );
-    wp_enqueue_style( 'bootstrap', $plugin_url . 'assets/css/bootstrap/bootstrap.min.css' );
-    wp_enqueue_style( 'free-quote', $plugin_url . 'assets/css/movers-quote.css' );
-    wp_enqueue_style( 'bootstrap-datepicker', $plugin_url . 'assets/css/bootstrap/datepicker/bootstrap-datepicker.min.css' );
-    wp_enqueue_style( 'bootstrap-selectpicker', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css' );
-     
+function load_plugin_css()
+{
+    $plugin_url = plugin_dir_url(__FILE__);
+    wp_enqueue_style('bootstrap', $plugin_url . 'assets/css/bootstrap/bootstrap.min.css');
+    wp_enqueue_style('free-quote', $plugin_url . 'assets/css/movers-quote.css');
+    wp_enqueue_style('bootstrap-datepicker', $plugin_url . 'assets/css/bootstrap/datepicker/bootstrap-datepicker.min.css');
+    wp_enqueue_style('bootstrap-selectpicker', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css');
 }
-add_action( 'wp_enqueue_scripts', 'load_plugin_css' );
+add_action('wp_enqueue_scripts', 'load_plugin_css');
 
 
 
@@ -191,4 +195,3 @@ function load_scripts()
     wp_enqueue_script('free-quote-js', plugins_url('assets/js/movers-quote.js', __FILE__), array('jquery'));
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
-?>
